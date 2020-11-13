18f08f7cd443a4812adda0d4cf7abd44dc831b36
justin <justin_yih@hotmail.com>
2020-11-12 20:11:12 -0800

    2

--- routes/authentication/login-signup-endpoint.js
+++ routes/authentication/login-signup-endpoint.js
@@ -73,46 +73,4 @@ router.post('/signup', async (req, res) => {
     }
 })
 
-// router.get('/login', async (req, res) => {
-//     //get form data and send to login handler
-//     try {
-//         //email and password
-//         await loginUser({email: 'test@test.com', password: 'best_password'})
-//             .then((user) => {
-//                 console.log(user)
-//                 return createNewToken({...user})
-//             })
-//             .then((token) => {
-//                 res.cookie('access_token', token, { maxAge: 999999999})
-//             })
-//             .then(() => {
-//                 //redirect here
-//                 res.send("logged in and cookie created.")
-//             })
-//         res.send()
-//     } catch (error) {
-//         res.send(error)
-//     }
-// })
-
-// router.get('/signup', async (req, res) => {
-//     //get form data and send to signup handler
-//     try {
-//         await signUpUser({first_name: 'justin', last_name: 'admin', birth_date: 'DOB', phone_number: '604-888-8888', email: 'test@test.com', password: 'best_password'})
-//             .then((newUser) => {
-//                 console.log(newUser)
-//                 return createNewToken({...newUser})
-//             })
-//             .then((token) => {
-//                 res.cookie('access_token', token, { maxAge: 999999999})
-//             })
-//             .then(() => {
-//                 //redirect here
-//                 res.send("signed up and cookie created.")
-//             })
-//     } catch (error) {
-//         res.send(error)
-//     }
-// })
-
 module.exports = router
