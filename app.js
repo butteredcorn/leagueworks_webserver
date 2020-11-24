f00ae0fd5b9a9ca3abac5871f961d5ceeed527b1
justin <justin_yih@hotmail.com>
2020-11-24 12:46:29 -0800

    socket connected

--- app.js
+++ app.js
@@ -29,35 +29,37 @@ module.exports = function () {
 
 
     //io logic
-    // io.use(async (socket, next) => {
-    //     try {
-    //         console.log("socket connected")
-    //         //console.log(socket.handshake.query)
-    //         if (socket.handshake.query && socket.handshake.query.token) {
-    //             //check for jwt token
-    //             const jwt = (socket.handshake.query.token).replace('access_token=', '')
-    //             const user = await verifyExistingToken(jwt)
-    //         } else {
-    //             throw new Error(`socket.handshake.query error. socket.handshake.query.token was ${socket.handshake.query.token}`)
-    //         }
-    //     } catch (err) {
-    //         console.log(err)
-    //     }
-    // })
+    io.use(async (socket, next) => {
+        try {
+            //console.log(socket.handshake.query)
+            console.log(socket.handshake.query)
+            // if (socket.handshake.query && socket.handshake.query.token) {
+            //     console.log(socket.handshake.query)
+            //     //check for jwt token
+            //     // const jwt = (socket.handshake.query.token).replace('access_token=', '')
+            //     // const user = await verifyExistingToken(jwt)
+            // } else {
+            //     throw new Error(`socket.handshake.query error. socket.handshake.query.token was ${socket.handshake.query.token}`)
+            // }
+            next()
+        } catch (err) {
+            console.log(err)
+        }
+    })
 
-    io.on("connection", (socket) => {
-        console.log("a user connected");
+    io.on('connection', (socket) => {
+        console.log('a user connected');
 
-        socket.emit("connection message", "hello from the server.")
+        socket.emit("connection", "Hello from the server.")
+
+        socket.on('react message', (message) => {
+            console.log(message)
+        })
+
+        socket.on('disconnect', () => {
+          console.log('user disconnected');
+        });
       });
-
-    io.on("message", (message) => {
-        console.log(message)
-    })
-
-    io.on("disconnect", (reason) => {
-        console.log(reason)
-    })
 
     //remaining general server logic
     app.get('/test', async (req, res) => {
